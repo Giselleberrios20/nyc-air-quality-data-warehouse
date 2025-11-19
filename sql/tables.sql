4029929ff1358f1bde10cc18bf7ac008d51b9c65
Giselle <giselleberrios0@gmail.com>
2025-11-19 13:33:22 -0500

    Update tables.sql

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,23 +1,33 @@
--- Create RAW air quality table
-CREATE TABLE raw_air_quality (
-    unique_id TEXT,
+CREATE TABLE dim_date (
+    date_key INT PRIMARY KEY,
+    time_period TEXT,
+    start_date DATE
+);
+
+CREATE TABLE dim_location (
+    location_key SERIAL PRIMARY KEY,
+    geo_type_name TEXT,
+    geo_join_id TEXT,
+    geo_place_name TEXT
+);
+
+CREATE TABLE dim_indicator (
+    indicator_key SERIAL PRIMARY KEY,
     indicator_id INT,
     name TEXT,
     measure TEXT,
-    measure_info TEXT,
-    geo_type_name TEXT,
-    geo_join_id TEXT,
-    geo_place_name TEXT,
-    time_period TEXT,
-    start_date TIMESTAMP,
-    data_value FLOAT,
-    message TEXT
+    measure_info TEXT
 );
 
--- Create PROCESSED air quality table
-CREATE TABLE processed_air_quality (
-    indicator_id INT,
-    geo_place_name TEXT,
-    time_period TEXT,
-    avg_value FLOAT
+CREATE TABLE fact_air_quality (
+    fact_id SERIAL PRIMARY KEY,
+    unique_id TEXT,
+    indicator_key INT,
+    location_key INT,
+    date_key INT,
+    data_value FLOAT,
+    message TEXT,
+    FOREIGN KEY (indicator_key) REFERENCES dim_indicator(indicator_key),
+    FOREIGN KEY (location_key) REFERENCES dim_location(location_key),
+    FOREIGN KEY (date_key) REFERENCES dim_date(date_key)
 );
